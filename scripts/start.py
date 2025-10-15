7e93988e3145f5f341cf04a76b6955a6eeb90cf3
Matthieu Baric <matthieu.baric@gmail.com>
2025-10-15 12:07:50 +0200

    first commit

---
+++ scripts/start.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Chargement
+df = pd.read_csv("data/5-Camp_Market.csv", sep=';')
+
+# Inspection initiale
+print("=" * 50)
+print("APERÇU DU DATASET")
+print("=" * 50)
+print(df.head())
+print("\n" + "=" * 50)
+print("INFORMATIONS GÉNÉRALES")
+print("=" * 50)
+print(df.info())
+print("\n" + "=" * 50)
+print("VALEURS MANQUANTES")
+print("=" * 50)
+print(df.isnull().sum())
+print("\n" + "=" * 50)
+print("STATISTIQUES DESCRIPTIVES")
+print("=" * 50)
+print(df.describe())
